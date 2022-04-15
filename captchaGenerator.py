e0be521fdf2804e34cc1bd246b8bc2d91b8c4b0c
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2022-04-15 08:38:08 +0100

    Modified main.py, created captchaGenerator.py

---
+++ captchaGenerator.py
@@ -0,0 +1,16 @@
+# To create the captcha
+from captcha.image import ImageCaptcha
+# To generate a random string
+from string import ascii_uppercase, ascii_lowercase, digits
+from random import sample, shuffle, randint
+
+def new_captcha():
+  image = ImageCaptcha(width = 250, height = 130, fonts=['./Jost/static/Jost-Medium.ttf'])
+
+  initial_password = sample(digits, randint(3, 5))
+  shuffle(initial_password)
+  final_password = "".join(initial_password)
+
+  data = image.generate(final_password)
+
+  image.write(final_password, 'output.png')
--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import os
 import discord
 from discord.ext import commands
 from discord.utils import get
+from discord import Embed
+
+from captchaGenerator import new_captcha
 
 from dotenv import load_dotenv
 
@@ -13,6 +16,9 @@ BOT_TOKEN = os.getenv('TOKEN')
 
 intents = discord.Intents.default()
 intents.members = True
+# intents.guilds = True
+# intents.messages = True
+# intents.reactions = True
 
 bot = commands.Bot(command_prefix="?", intents=intents)
 
@@ -28,9 +34,27 @@ async def joined(ctx, member: discord.Member):
 
 @bot.event
 async def on_member_join(member):
+  guild = member.guild
+  if guild.system_channel is not None:
+    to_send = f"Welcome {member.mention} to {guild.name}!"
+    await guild.system_channel.send(to_send)
   # channel = get(ctx.guild.channels,name="Welcome")
   # await channel.send(f"{member.mention} has joined")
-  await member.send(f'Welcome to the server, {member.mention}! Enjoy your stay here.\nSend a **TASK**')
+  new_captcha()
+  welcome_message = Embed(
+    title=f"{guild.name} Verification Bot",
+    description=f"""
+      Welcome to {guild.name}, {member.mention}!
+      \nWe hope you enjoy your stay here but we need you to complete verification below.
+      \nPlease enter the numbers you see in the image.
+    """
+  )
+  try:
+    file = discord.File("./output.png")
+    welcome_message.set_image(url="attachment://output.png")
+    await member.send(file = file, embed=welcome_message)
+  except:
+    await member.send(embed=welcome_message)
 
 @bot.event
 async def on_message(message):
